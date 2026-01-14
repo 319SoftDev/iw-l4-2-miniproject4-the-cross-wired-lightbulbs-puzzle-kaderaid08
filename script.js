53bf100315cbbdba4c7e6d26c27fe8e8a7fc3aef
kaderaid08 <karinar97@nycstudents.net>
2026-01-14 10:41:19 -0500

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // YOUR CODE HERE
 console.log("Script Running.");
 const switch1 = document.querySelector("#switch1");
-// const switch2 = document.querySelector("#switch2");
-// const switch3 = document.querySelector("#switch3");
-// const switch4 = document.querySelector("#switch4");
-// const switch5 = document.querySelector("#switch5");
-// const switch6 = document.querySelector("#switch6");
-// const switch7 = document.querySelector("#switch7");
-// const switch8 = document.querySelector("#switch8");
-// const switch9 = document.querySelector("#switch9");
+const switch2 = document.querySelector("#switch2");
+const switch3 = document.querySelector("#switch3");
+const switch4 = document.querySelector("#switch4");
+const switch5 = document.querySelector("#switch5");
+const switch6 = document.querySelector("#switch6");
+const switch7 = document.querySelector("#switch7");
+const switch8 = document.querySelector("#switch8");
+const switch9 = document.querySelector("#switch9");
 
 const light1 = document.querySelector("#lightbulb1")
 const light2 = document.querySelector("#lightbulb2")
@@ -19,9 +19,8 @@ const light6 = document.querySelector("#lightbulb6")
 const light7 = document.querySelector("#lightbulb7")
 const light8 = document.querySelector("#lightbulb8")
 const light9 = document.querySelector("#lightbulb9")
-const lightbulb = document.querySelector(".light");
-
-
+const master = document.querySelector("#master")
+const restart = document.querySelector("#restart")
 
 //switch buddies? (singles, pairs, and trios)
 const change38 = () => {
@@ -93,6 +92,49 @@ switch9.addEventListener("click", change67);
 
 //master switch
 const masterSwitch = () => {
-    lightbulb.classList.toggle("active");
+    light1.classList.toggle("active");
+    light2.classList.toggle("active");
+    light3.classList.toggle("active");
+    light4.classList.toggle("active");
+    light5.classList.toggle("active");
+    light6.classList.toggle("active");
+    light7.classList.toggle("active");
+    light8.classList.toggle("active");
+    light9.classList.toggle("active");
 }
-light1.addEventListener("click", masterSwitch)
+master.addEventListener("click", masterSwitch);
+
+//restart button
+
+const restartButton = () => {
+    light1.classList.remove("active");
+    light2.classList.remove("active");
+    light3.classList.remove("active");
+    light4.classList.remove("active");
+    light5.classList.remove("active");
+    light6.classList.remove("active");
+    light7.classList.remove("active");
+    light8.classList.remove("active");
+    light9.classList.remove("active");
+
+    switch1.checked=false;
+    switch2.checked=false;
+    switch3.checked=false;
+    switch4.checked=false;
+    switch5.checked=false;
+    switch6.checked=false;
+    switch7.checked=false;
+    switch8.checked=false;
+    switch9.checked=false;
+}
+restart.addEventListener("click", restartButton);
+
+
+//randomize (SPICY)
+
+const randomize = () =>{
+    if (switch.checked){
+
+    }
+}
+
